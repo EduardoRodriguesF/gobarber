4b062dcb17b15d7c479e10b1d8d9e08008a74f2f
EduardoRodriguesF <eduaadorfernandes@gmail.com>
2020-12-01 11:07:24 -0300

    chore: liskov substitution principle applied

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -2,10 +2,10 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppError from '@shared/errors/AppError';
 
-import AppError from '../../../shared/errors/AppError';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
 
 // DTO
 interface Request {
