80a9f944e40f5e561e76e61db7bb8fd3c4dedfdb
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2020-12-02 07:14:03 -0300

    chore: users repository refactored

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -8,7 +8,6 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -25,7 +24,10 @@ appointmentsRouter.post('/', async (request, response) => {
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService(appointmentsRepository);
+  const appointmentsRepository = new AppointmentsRepository();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
--- backend/src/modules/users/infra/http/routes/sessions.routes.ts
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,26 +1,28 @@
 import { Router } from 'express';
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-    try {
-        const { email, password } = request.body;
+  try {
+    const { email, password } = request.body;
 
-        const authenticateUser = new AuthenticateUserService();
+    const usersRepository = new UsersRepository();
+    const authenticateUser = new AuthenticateUserService(usersRepository);
 
-        const { user, token } = await authenticateUser.execute({
-            email,
-            password,
-        });
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password,
+    });
 
-        delete user.password;
+    delete user.password;
 
-        return response.json({ user, token });
-    } catch (err) {
-        return response.status(err.statusCode).json({ error: err.message });
-    }
+    return response.json({ user, token });
+  } catch (err) {
+    return response.status(err.statusCode).json({ error: err.message });
+  }
 });
 
 export default sessionsRouter;
