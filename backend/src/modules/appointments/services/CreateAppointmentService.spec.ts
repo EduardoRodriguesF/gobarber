5e41a97c3b79665dd8c1c6fd11ebcdbca29024f5
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2020-12-02 13:25:56 -0300

    test: set coverage report

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,18 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await createAppointment.execute({ provider_id: '1234512', date: new Date() })
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1234512');
+  });
+
+  /* it('should not be able to create two appointment on the same time', () => {
+    expect(1 + 2).toBe(3);
+  }); */
+});
