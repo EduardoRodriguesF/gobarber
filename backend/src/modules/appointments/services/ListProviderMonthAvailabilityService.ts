94954818b8e0aa2c00a2a664d4eeb0af7fb723ab
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2020-12-07 10:23:29 -0300

    feat: listing available days

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -22,15 +23,31 @@ class ListProviderMonthAvailabilityService {
   ) {}
 
   public async execute({ provider_id, year, month }: IRequest): Promise<IResponse> {
-    const appointments = this.appointmentsRepository.findAllInMonthFromProvider({
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider({
       provider_id,
       year,
       month,
     });
 
-    console.log(appointments)
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
