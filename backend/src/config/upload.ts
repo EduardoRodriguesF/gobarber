64bb334f16fc65560c7be5fe7fffbc7ef1c0c3fe
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2020-12-09 08:29:31 -0300

    implementation(storageProvider): Amazon S3

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,20 +1,46 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine
+  }
+
+  config: {
+    disk: {}
+  }
+}
+
 export default {
-    tmpFolder: tmpFolder,
-    uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+  driver: process.env.STORAGE_DRIVER,
 
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+
+  multer: {
     storage: multer.diskStorage({
-        destination: tmpFolder,
-        filename(request, file, callback) {
-            const fileHash = crypto.randomBytes(10).toString('hex');
-            const fileName = `${fileHash}-${file.originalname}`;
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-            return callback(null, fileName);
-        },
+        return callback(null, fileName);
+      },
     }),
-};
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'eduardo-application-gobarber',
+    }
+  }
+} as IUploadConfig;
