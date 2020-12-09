c02d708ac3b0f739cfef51fa38527b33f3e27b1d
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2020-12-09 07:43:56 -0300

    implementation(mailProvider): Amazon SES

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    }
+  }
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      name: 'Eduardo da GoBarber',
+      email: 'contato@edurodrigues.dev',
+    }
+  }
+} as IMailConfig;
