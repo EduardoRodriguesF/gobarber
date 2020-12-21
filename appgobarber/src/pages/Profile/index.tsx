ec61a5ee5bc10cc91d5d9a8c6f8f9366cba09236
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2020-12-21 10:33:56 -0300

    feat(page): profile

--- appgobarber/src/pages/Profile/index.tsx
+++ appgobarber/src/pages/Profile/index.tsx
@@ -1,10 +1,162 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef, useCallback } from 'react';
+import { View, KeyboardAvoidingView, Platform, ScrollView, TextInput, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import * as Yup from 'yup';
+import api from '../../services/api';
 
-const Profile: React.FC = () => {
-  return (
-    <View />
-  )
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, UserAvatarButton, UserAvatar, BackButton } from './styles';
+import { useAuth } from '../../hooks/auth';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
 }
 
-export default Profile;
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation();
+  const { user } = useAuth();
+  const placeholderProfile = 'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?b=1&k=6&m=1214428300&s=612x612&w=0&h=kMXMpWVL6mkLu0TN-9MJcEUx1oSWgUq8-Ny6Wszv_ms=';
+
+  const emailInputRef = useRef<TextInput>(null);
+  const oldPasswordInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const confirmPasswordInputRef = useRef<TextInput>(null);
+
+  const handleSignUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 linhas'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await api.post('/users', data);
+
+      Alert.alert('Cadastro realizado com sucesso', 'Você já pode fazer login na aplicação.')
+
+      navigation.goBack();
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert('Erro no cadastro', 'Ocorreu um erro ao fazer cadastro, tente novamente.');
+    }
+  }, [navigation]);
+
+  const handleGoBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <BackButton onPress={handleGoBack}>
+              <Icon name="chevron-left" size={24} color="#999591" />
+            </BackButton>
+
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={{ uri: user.avatar_url || placeholderProfile}} />
+            </UserAvatarButton>
+
+            <View>
+              <Title>Meu perfil</Title>
+            </View>
+
+            <Form ref={formRef} onSubmit={handleSignUp}>
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus()
+                }}
+              />
+
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus()
+                }}
+              />
+
+              <Input
+                ref={oldPasswordInputRef}
+                secureTextEntry
+                name="old_password"
+                icon="lock"
+                placeholder="Senha atual"
+                textContentType="newPassword"
+                returnKeyType="next"
+                ContainerStyle={{ marginTop: 16 }}
+                onSubmitEditing={() => oldPasswordInputRef.current?.focus()}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                name="password"
+                icon="lock"
+                placeholder="Nova senha"
+                textContentType="newPassword"
+                returnKeyType="next"
+                onSubmitEditing={() => passwordInputRef.current?.focus()}
+              />
+
+              <Input
+                ref={confirmPasswordInputRef}
+                secureTextEntry
+                name="confirm_password"
+                icon="lock"
+                placeholder="Confirmar senha"
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => confirmPasswordInputRef.current?.focus()}
+              />
+
+              <Button onPress={() => formRef.current?.submitForm()}>Confirmar mudanças</Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
+  );
+}
+
+export default SignUp;
